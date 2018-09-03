1cdb7d033bb793196c1e1b7c7c378aa4e721eda7
Draww <drawwbeta@gmail.com>
2018-09-03 15:39:21 +0300

    initial commit

---
+++ src/main/java/io/github/draww/fastenchant/FastEnchant.java
@@ -0,0 +1,59 @@
+package io.github.draww.fastenchant;
+
+import co.aikar.commands.BukkitCommandManager;
+import io.github.draww.fastenchant.utils.StringUtils;
+import me.blackness.black.Blackness;
+import org.bukkit.event.HandlerList;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import static io.github.draww.fastenchant.utils.StringUtils.*;
+
+public class FastEnchant extends JavaPlugin {
+
+    private static FastEnchant instance;
+
+    private BukkitCommandManager commandManager;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        new Blackness().prepareFor(this);
+        commandManager = new BukkitCommandManager(this);
+        log("*--------------------------------------------*");
+        log("| FastEnchant has been successfully enabled! |");
+        log("*--------------------------------------------*");
+    }
+
+    @Override
+    public void onDisable() {
+        log("*--------------------------------*");
+        log("| FastEnchant has been disabled! |");
+        log("*--------------------------------*");
+        HandlerList.unregisterAll(this);
+        commandManager.unregisterCommands();
+    }
+
+    public static FastEnchant getInstance() {
+        return instance;
+    }
+
+    public BukkitCommandManager getCommandManager() {
+        return commandManager;
+    }
+
+    public void log(String string) {
+        getLogger().info(() -> string);
+    }
+
+    public void logDebug(Object object) {
+        getLogger().info(() -> "DEBUG: " + object);
+    }
+
+    public void logError(String error) {
+        getLogger().severe(() -> error);
+    }
+
+    public void logWarning(String warning) {
+        getLogger().warning(warning);
+    }
+}
---
+++ src/main/java/io/github/draww/fastenchant/utils/StringUtils.java
@@ -0,0 +1,11 @@
+package io.github.draww.fastenchant.utils;
+
+import org.bukkit.ChatColor;
+
+public class StringUtils {
+
+    public static String cc(String text) {
+        return ChatColor.translateAlternateColorCodes('&', text);
+    }
+
+}
